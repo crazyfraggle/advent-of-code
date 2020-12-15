3d9b28aa00f3d1bf3dd8b051657e2505c64c098e
Frank Oppervoll-Larsen <frlarsen@microsoft.com>
2020-12-15 10:02:11 +0100

    Day 14 part deux

--- 14/task.js
+++ 14/task.js
@@ -3,8 +3,8 @@ const filename = process.argv[2] || "testdata.txt";
 
 const lines = ir.lineReader(filename);
 
-const mask0base = 0xfffffffff;
-const mask1base = 0x000000000;
+const mask0base = 0xfffffffffn;
+const mask1base = 0x000000000n;
 
 let mask0 = mask0base;
 let mask1 = mask1base;
@@ -22,10 +22,62 @@ for (let i = 0; i < lines.length; i++) {
     mask0 = BigInt(parseInt(mask.replace(/X/g, "1"), 2));
     mask1 = BigInt(parseInt(mask.replace(/X/g, "0"), 2));
   } else {
-    const [_, pos, val] = memRe.exec(line);
-    memory[pos] = (BigInt(val) & mask0) | mask1;
+    const [_, address, val] = memRe.exec(line);
+    memory[address] = (BigInt(val) & mask0) | mask1;
   }
 }
 console.table(memory);
 const total = memory.reduce((sum, cell) => (sum = sum + cell), 0n);
 console.log("Sum of memory cells:", total);
+
+// Part 2
+
+function explodeAddress(address, mask) {
+  const binaryAddress = BigInt(address).toString(2);
+  let outputAddresses = [new Array(36).fill("0")];
+  binaryAddress.split("").forEach((d, idx, arr) => {
+    outputAddresses[0][36 - arr.length + idx] = d;
+  });
+  // console.log(address, mask, JSON.stringify(outputAddresses));
+  mask.split("").forEach((bitOp, idx) => {
+    if (bitOp === "1") {
+      outputAddresses.forEach((oa) => (oa[idx] = "1"));
+    } else if (bitOp === "X") {
+      const newAddresses = [];
+      outputAddresses.forEach((oa) => {
+        oa[idx] = "0";
+        const na = oa.slice();
+        na[idx] = "1";
+        newAddresses.push(na);
+      });
+      outputAddresses = outputAddresses.concat(newAddresses);
+    }
+  });
+  // console.log(address, mask, JSON.stringify(outputAddresses));
+  outputAddresses = outputAddresses.map((oa) => parseInt(oa.join(""), 2));
+  // console.log(address, mask, JSON.stringify(outputAddresses));
+  return outputAddresses;
+}
+
+const mem2 = {};
+let mask = "000000000000000000000000000000000000";
+for (let i = 0; i < lines.length; i++) {
+  const line = lines[i];
+  if (maskRe.test(line)) {
+    [_, mask] = line.split(" = ");
+    // console.log("mask", mask);
+  } else {
+    const [_, address, val] = memRe.exec(line);
+    explodeAddress(address, mask).forEach((addr) => {
+      mem2[addr] = parseInt(val);
+    });
+  }
+}
+// console.table(mem2);
+const writtenCells = Object.keys(mem2);
+console.log("Number of written cells:", writtenCells.length);
+let tot2 = 0;
+writtenCells.forEach((cell) => {
+  tot2 += mem2[cell];
+});
+console.log("Sum of memory cells:", tot2);
